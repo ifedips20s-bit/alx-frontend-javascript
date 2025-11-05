2eaa9e3b562244563b95384a38a2746fbbf13027
ifedips20s <ifedips20s@gmail.com>
2025-11-05 23:51:40 +0100

    Add corrections to main file

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,65 @@
+// Director Interface
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+// Teacher Interface
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// Director class implementing DirectorInterface
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+// Teacher class implementing TeacherInterface
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+// Factory function
+function createEmployee(salary: number | string): Director | Teacher {
+  let numericSalary: number;
+
+  if (typeof salary === "string") {
+    numericSalary = parseInt(salary.replace(/\D/g, ""), 10);
+  } else {
+    numericSalary = salary;
+  }
+
+  if (numericSalary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+// Testing outputs
+console.log(createEmployee(200)); // Teacher
+console.log(createEmployee(1000)); // Director
+console.log(createEmployee("$500")); // Director
